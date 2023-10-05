157c6689874d25af940b39fa9bbc1e5d1cfcf591
mcdott <111994803+mcdott@users.noreply.github.com>
2023-10-05 15:12:28 -0400

    Replaces 'About' page with blog post

--- app.py
+++ app.py
@@ -38,10 +38,10 @@ def page_not_found(e):
     """Displays a custom 404 error page if the user enters an invalid URL"""
     return render_template('404.html'), 404
 
-@app.route('/about')
-def about():
-    """Display the about page."""
-    return render_template('about.html')
+@app.route('/blog')
+def blog():
+    """Display the blog page."""
+    return render_template('blog.html')
 
 @app.route('/create', methods=['GET', 'POST'])
 def create():
